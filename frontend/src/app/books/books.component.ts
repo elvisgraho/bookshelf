fb09c1f5613c0a67675556182fd0ca6f05de1fe2
Elvis <elvis.graholskis@cloudbrixx.de>
2021-11-08 15:42:18 +0100

    Search Books

--- frontend/src/app/books/books.component.ts
+++ frontend/src/app/books/books.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Book, BooksService } from './services/books.service';
 
@@ -10,6 +10,9 @@ import { Book, BooksService } from './services/books.service';
 export class BooksComponent implements OnInit, OnDestroy {
   public bookSub!: Subscription;
   public books: Book[] = [];
+  public booksDisplay: Book[] = [];
+
+  private searchString: string = '';
 
   constructor(private booksService: BooksService) {}
 
@@ -19,10 +22,36 @@ export class BooksComponent implements OnInit, OnDestroy {
 
     this.bookSub = this.booksService.getBooksBS().subscribe((val) => {
       this.books = val;
+      this.filterSearchString();
     });
   }
 
   ngOnDestroy() {
     this.bookSub && this.bookSub.unsubscribe();
+  }
+
+  public onSearchInput($event: any) {
+    this.searchString = $event.target.value;
+
+    this.searchString = this.searchString.toLowerCase();
+
+    this.filterSearchString();
+  }
+
+  private filterSearchString() {
+    if (!this.searchString || this.searchString.length == 0) {
+      this.booksDisplay = [...this.books];
+      return;
+    }
+
+    const newList: Book[] = [];
+    for (const book of this.books) {
+      const bookString: string = `${book.isbn} ${book.name} ${book.authors}`.toLowerCase();
+      if (bookString.includes(this.searchString)) {
+        newList.push(book);
+      }
+    }
+
+    this.booksDisplay = newList;
   }
 }
